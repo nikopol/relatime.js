7d614dba3d2c598b94f9c012a801f692df5c4b07
niko <nikomomo@gmail.com>
2013-11-15 16:19:31 +0100

    refactoring

--- relatime.js
+++ relatime.js
@@ -1,17 +1,15 @@
-// reltime.js 0.1 - relative date display
+// relatime.js 0.2 - relative date time display and update
 // https://github.com/nikopol/reltime.js
 // niko 2013
 
 /*
-
-display dates in a relative form.
-eg: relatime.text('2013-11-14T13:24:43.310Z') => '2 minutes ago'.
+display dates in a relative form and automatically update them every minutes.
 
 methods:
 
 setup(opt)      setup relatime parameters :
                   opt.locale     (by default navigator.language or en)
-                  opt.tag        (by default "span")
+                  opt.tag        (by default "time")
                   opt.classname  tag.classname (by default "relatime")
                   opt.refresh    auto-updates interval in seconds (by default 60)
 
@@ -22,6 +20,8 @@ locale()        return the current locale
 start()         start the auto-update of displayed dates
                 by default auto-updates are automatically started
                 when html(date) is called
+
+start(opt)      same shit but with setup option provided
 
 stop()          stop the auto-update of displayed dates
 
@@ -34,7 +34,7 @@ text(date)      return a relative representation of date
 html(date)      return a relative representation of date
                 embedded in an auto-updated span
                 date can be a string or a Date object
-                eg: "<span class="relatime" data-date="2013-11-14T13:24:43.310Z" title="11/14/2013, 2:24:43 PM">one hour ago</span>"
+                eg: "<time class="relatime" datetime="2013-11-14T13:24:43.310Z" title="11/14/2013, 2:24:43 PM">one hour ago</span>"
 
 */
 
@@ -48,7 +48,8 @@ var relatime = (function(){
 			hour: {s:'one hour ago',p:'%d hours ago'},
 			day: {s:'yesterday',p:'%d days ago'},
 			week: {s:'last week',p:'%d weeks ago'},
-			month: {s:'last month',p:'%d months ago'}
+			month: {s:'last month',p:'%d months ago'},
+			year: {s:'one year ago',p:'%d years ago'}
 		},
 		fr: {
 			now: "a l'instant",
@@ -56,7 +57,8 @@ var relatime = (function(){
 			hour: {s:'il y a %d heure',p:'il y a %d heures'},
 			day: {s:'hier',p:'il y a %d jours'},
 			week: {s:'la semaine dernière',p:'il y a %d semaines'},
-			month: {s:'le mois dernier',p:'il y a %d mois'}
+			month: {s:'le mois dernier',p:'il y a %d mois'},
+			year: {s:'il y a un an',p:'il y a %d ans'}
 		},
 		de: {
 			now: 'soeben',
@@ -64,54 +66,22 @@ var relatime = (function(){
 			hour: {s:'vor einer Stunde',p:'vor %d Stunden'},
 			day: {s:'gestern',p:'vor %d Tagen'},
 			week: {s:'vor einer Woche',p:'vor %d Wochen'},
-			month: {s:'vor einem Monat',p:'vor %d Monaten'}
+			month: {s:'vor einem Monat',p:'vor %d Monaten'},
+			year: {s:'vor einem Jahr',p:'vor %d Jahren'}
 		}
 	},
 	loc = 'en',
-	tag = 'span',
+	tag = 'time',
 	classname = 'relatime',
 	timer = false,
 	autostart = true,
-	refresh = 60*1000,
-	locale = function(l) {
-		if(l!=undefined && dic[l]) loc=l;
-		return loc;
+	interval = 60*1000,
+	datobj = function(d) {
+		return d instanceof Date ? d : new Date(d);
 	},
-	stop = function() {
-		if(timer!==false) {
-			clearInterval(timer);
-			timer = false;
-		}
-	},
-	start = function() {
-		stop();
-		timer = setInterval(function(){
-			var i=0,e,d,nl = document.querySelectorAll(tag+'.'+classname);
-			while( i<nl.length ) {
-				e = nl[i];
-				d = e.getAttribute('data-date');
-				if(d) e.innerText = text(epoc(d));
-				++i;
-			}
-		}, 1*1000);
-	},
-	started = function() {
-		return timer!==false;
-	},
-	setup = function(opt) {
-		opt.tag && (tag=opt.tag);
-		opt.classname && (classname=opt.classname);
-		opt.locale && locale(opt.locale);
-		opt.lang && locale(opt.lang);
-		if( opt.refresh ) {
-			refresh=1000*opt.refresh;
-			if(started()) start();
-		}
-		(opt.autostart!==undefined) && (autostart=opt.autostart);
-	},
-	epoc = function(d) {
+	delta = function(d) {
 		var
-		d1 = d instanceof Date ? d : new Date(d),
+		d1 = datobj(d),
 		d2 = new Date();
 		return Math.floor((d2.getTime() - d1.getTime()) / 1000);
 	},
@@ -119,34 +89,76 @@ var relatime = (function(){
 		n = Math.round(n);
 		return dic[loc][t][n>1?'p':'s'].replace('%d',n);
 	},
-	text = function(e) {
-		if(e<60)      return dic[loc].now;
-		if(e<3600)    return fmt('min',e/60); 
-		if(e<86400)   return fmt('hour',e/3600);
-		if(e<604800)  return fmt('day',e/86400);
-		if(e<2592000) return fmt('week',e/604800);
-		return fmt('month',e/2592000);
+	text = function(d,_){
+		var e = _?_:delta(d);
+		if(e<60)       return dic[loc].now;
+		if(e<3600)     return fmt('min',e/60); 
+		if(e<86400)    return fmt('hour',e/3600);
+		if(e<604800)   return fmt('day',e/86400);
+		if(e<2592000)  return fmt('week',e/604800);
+		if(e<31536000) return fmt('month',e/2592000);
+		return fmt('year',e/31536000);
 	},
-	html = function(s) {
-		var
-		d = s instanceof Date ? s : new Date(s),
-		e = epoc(d),
-		t = text(e);
-		if(e>2592000) return '<'+tag+' title="'+d.toLocaleString()+'">'+t+'</'+tag+'>';
-		if(autostart && !started()) start();
-		return '<'+tag+' class="'+classname+'" data-date="'+d.toISOString()+'" title="'+d.toLocaleString()+'">'+t+'</'+tag+'>';
+	refresh = function(){
+		var e, d, i=0, nl = document.querySelectorAll(tag+'.'+classname);
+		console.log('[relatime] refreshing',nl.length,'date');
+		while( i<nl.length ) {
+			e = nl[i];
+			d = e.getAttribute('datetime');
+			if(d) e.innerText = text(d);
+			++i;
+		}
+	},
+	locale = function(l){
+		if(l!=undefined && l!=loc && dic[l]) {
+			loc=l;
+			setTimeout(refresh,9);
+		}
+		return loc;
 	};
 
 	locale(navigator.language);
 
 	return {
-		setup: setup,
-		start: start,
-		stop: stop,
+		setup: function(opt){
+			opt.tag && (tag=opt.tag);
+			opt.classname && (classname=opt.classname);
+			opt.locale && locale(opt.locale);
+			if( opt.interval ) {
+				interval=1000*opt.interval;
+				if(this.started()) this.start();
+			}
+			(opt.autostart!==undefined) && (autostart=opt.autostart);
+			return this;
+		},
 		locale: locale,
-		started: started,
-		text: function(d,o){ o && setup(o); return text(epoc(d)); },
-		html: function(d,o){ o && setup(o); return html(d); }
+		start: function(opt){
+			if(opt) this.setup(opt);
+			this.stop();
+			if(interval>500) refresh();
+			timer = setInterval(refresh,interval);
+			return this;
+		},
+		started: function(){
+			return timer!==false;
+		},
+		stop: function(){
+			if(timer!==false) {
+				clearInterval(timer);
+				timer = false;
+			}
+			return this;
+		},
+		text: text,
+		html: function(s) {
+			var
+			d = datobj(s),
+			e = delta(d),
+			t = text(d,e);
+			if(e>2592000) return '<'+tag+' title="'+d.toLocaleString()+'">'+t+'</'+tag+'>';
+			if(autostart && !this.started()) this.start();
+			return '<'+tag+' class="'+classname+'" datetime="'+d.toISOString()+'" title="'+d.toLocaleString()+'">'+t+'</'+tag+'>';
+		}
 	};
 
 })();
